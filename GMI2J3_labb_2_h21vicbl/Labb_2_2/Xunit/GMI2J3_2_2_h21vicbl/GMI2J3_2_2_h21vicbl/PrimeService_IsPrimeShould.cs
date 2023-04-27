f0ea06952716b0b3e85e4fdad2ffdc4822d08d02
Tranzzid <96055621+Tranzzid@users.noreply.github.com>
2023-04-27 16:25:58 +0200

    Initiate lab_2_2

---
+++ GMI2J3_labb_2_h21vicbl/Labb_2_2/Xunit/GMI2J3_2_2_h21vicbl/GMI2J3_2_2_h21vicbl/PrimeService_IsPrimeShould.cs
@@ -0,0 +1,13 @@
+
+
+namespace GMI2J3_2_2_h21vicbl
+{
+    public class PrimeService_IsPrimeShould
+    {
+        [Fact]
+        public void IsPrime_InputIs1_ReturnFalse()
+        {
+            var primeService = PrimeService();
+        }
+    }
+}
---
+++ GMI2J3_labb_2_h21vicbl/Labb_2_2/Xunit/GMI2J3_2_2_h21vicbl/PrimeService/PrimeService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace GMI2J3_2_2_h21vicbl
+{
+    public class PrimeService
+    {
+        public bool IsPrime(int candidate)
+        {
+            throw new NotImplementedException("Not implemented");
+        }
+
+    }
+}
